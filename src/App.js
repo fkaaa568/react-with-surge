7ace8d248d914e09c1205cfcec2348c2f2d5a428
Faizan Ali <fkaaa567@gmail.com>
2020-10-13 00:23:34 +0500

    update checkout version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h1>Hello World</h1>
-    <p>hahhahahhaahhahhahahahhaha</p>
+    <p>hahhahahhaahhahhahahahhahaa</p>
     </div>
   );
 }
